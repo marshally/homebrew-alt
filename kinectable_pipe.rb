ff3d55db0a0bb835a22cad935d8650901b0dda48
Marshall Yount <myount@zynga.com>
2012-06-21 10:16:27 -0500

    Fixes #1

--- kinectable_pipe.rb
+++ kinectable_pipe.rb
@@ -4,7 +4,7 @@ class KinectablePipe < Formula
   homepage 'https://github.com/marshally/kinectable_pipe'
   url 'https://github.com/marshally/kinectable_pipe/tarball/master'
   version '1.0'
-  md5 'f99858bf86bb26efc9d4bf9555de9156'
+  md5 'a9662ee3e4756319d621a5bf5c7ac720'
 
   depends_on 'sensor_kinect'
 
