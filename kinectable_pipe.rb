6448eaf9f7792de02714e82d00d27b8207ad1af6
Marshall Yount <marshall@yountlabs.com>
2012-06-20 21:54:51 -0500

    add md5 for kinectable_pipe

--- kinectable_pipe.rb
+++ kinectable_pipe.rb
@@ -4,11 +4,12 @@ class KinectablePipe < Formula
   homepage 'https://github.com/marshally/kinectable_pipe'
   url 'https://github.com/marshally/kinectable_pipe/tarball/master'
   version '1.0'
+  md5 'f99858bf86bb26efc9d4bf9555de9156'
 
   depends_on 'sensor_kinect'
 
   def install
-    system "make clean"
+    system 'make clean'
     bin.install 'kinectable_pipe'
   end
 end
