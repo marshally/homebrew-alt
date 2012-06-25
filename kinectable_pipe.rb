c19c25eedb586beee2ac589be88a152c842b9067
Marshall Yount <marshall@yountlabs.com>
2012-06-25 09:24:29 -0500

    downgrade version number to 0.0.1 for better flexibility

--- kinectable_pipe.rb
+++ kinectable_pipe.rb
@@ -3,7 +3,7 @@ require 'formula'
 class KinectablePipe < Formula
   homepage 'https://github.com/marshally/kinectable_pipe'
   url 'https://github.com/marshally/kinectable_pipe/tarball/master'
-  version '1.0'
+  version '0.0.1'
   md5 'a9662ee3e4756319d621a5bf5c7ac720'
 
   depends_on 'sensor_kinect'
