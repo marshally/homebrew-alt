4e97d905e78555bfa2e3fbe0e0484952e467a32b
Marshall Yount <myount@zynga.com>
2012-08-14 07:25:00 -0500

    use libpng12 from here until homebrew-versions is fixed

--- png++.rb
+++ png++.rb
@@ -6,7 +6,7 @@ class Pngxx < Formula
   sha1 'bfe39c414a8c5c18dbaaf96fea22ce12a6887576'
   version '0.2.5'
 
-  depends_on 'libpng'
+  depends_on 'marshally/alt/libpng12' => :alt
 
   def install
     include.install Dir['*.hpp']
